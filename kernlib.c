8e1f2adecfb19a1af9b2bb66463569f982931013
ian <ian@bb4361fd-baa2-43a2-a164-8bb2ddc2f287>
2008-11-11 20:14:00 +0000

    Added a very simple KWriteHex.

--- kernlib.c
+++ kernlib.c
@@ -63,3 +63,42 @@ void KWriteString(char * str, int row, int col)
 	asm("sti");
 }
 
+/*
+==========================================================================
+A kernel library function to write a quad character to the screen.
+==========================================================================
+*/
+void KWriteHex(long c) //, int row, int col)
+{
+	char * VideoBuffer = (char *)0xB8000;
+
+	//asm("cli");
+	asm("push %rax");
+	asm("push %rbx");
+	asm("push %rcx");
+	asm("push %rdx");
+	asm("push %rdi");
+	asm("push %rsi");
+	int i;
+	for (i == 0; i < 8; i++)
+	{	
+		char lo = c & 0xF;
+		lo += 0x30;
+		if (lo > 0x39)  lo += 7;
+		char hi = (c & 0xF0) >> 4;
+		hi += 0x30;
+		if (hi > 0x39) hi += 7;
+		VideoBuffer[28 - 4 * i] = hi;
+		VideoBuffer[30 - 4 * i] = lo;
+		c = c >> 8;
+	}
+	asm("pop %rsi");
+	asm("pop %rdi");
+	asm("pop %rdx");
+	asm("pop %rcx");
+	asm("pop %rbx");
+	asm("pop %rax");
+	//asm("sti");
+}
+
+
