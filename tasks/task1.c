7ef63362018b6eebc6eb1e16323ec34cabd6d944
ian <ian@bb4361fd-baa2-43a2-a164-8bb2ddc2f287>
2008-11-13 16:33:59 +0000

    Starting to turn task1 into an interactive shell.

--- tasks/task1.c
+++ tasks/task1.c
@@ -2,42 +2,75 @@
 #include "cmemory.h"
 #include "library/syscalls.h"
 
+char getchar(void);
+
 void main(void)
 {
 	struct Message *taskMsg;
-	struct Message *kbdMsg;
+	int row = 0;
+	int column = 0;
+	char commandline[12];
 
-	//ClearScr();
-	WriteString("Goodbye Cruel 32-bit World! Hello 64-bit.", 10, 0);
+	ClearScr();
+	WriteString("Goodbye Cruel 32-bit World! Hello 64-bit.", 0, 0);
+	row++;
+	WriteString("#> ", row, 0);
+	column = 3;
+	WriteChar('_', row, column);
 	sys_CreateTask("TASK2   BIN");
 	while (1)
 	{
-		kbdMsg = (struct Message *)sys_AllocMem(sizeof(struct Message));
-		kbdMsg->nextMessage = 0;
-		kbdMsg->byte = 1;
-		kbdMsg->quad = 0;
-		SendReceive(KbdPort, kbdMsg);
-		char c = kbdMsg->byte;
-		sys_DeallocMem(kbdMsg);
-		WriteChar(c, 11, 0);
+		char c = getchar();
 		switch (c)
 		{
-			case 't':
-				sys_CreateTask("TASK3   BIN");
+			//case 'p':
+			//	taskMsg = (struct Message *)sys_AllocMem(sizeof(struct Message));
+			//	taskMsg->nextMessage = 0;
+			//	taskMsg->byte = 0x12;
+			//	taskMsg->quad = 0x9876543212345678;
+			//	Send(StaticPort, taskMsg);
+			//	sys_DeallocMem(taskMsg);
+			//	break;
+			case 13:
+				WriteString(commandline, 23, 0);
+				column = 0;
+				row++;
+				WriteString("#> ", row, 0);
+				column = 3;
+				WriteChar('_', row, column);
+				// Convert commandline[] to uppercase.
+				char i;
+				for (i = 0; i < 12; i++)
+				{
+					if (commandline[i] >= 'a' & commandline[i] <= 'z') 
+						commandline[i] = commandline[i] - 0x20;
+				}
+				WriteString(commandline, 24, 0);
+				sys_CreateTask(commandline);
 				break;
-			case 'p':
-				taskMsg = (struct Message *)sys_AllocMem(sizeof(struct Message));
-				taskMsg->nextMessage = 0;
-				taskMsg->byte = 0x12;
-				taskMsg->quad = 0x9876543212345678;
-				Send(StaticPort, taskMsg);
-				sys_DeallocMem(taskMsg);
-				break;
-			case 'k':
-				sys_KillTask();
 			default:
+				WriteDouble(column, 24, 20);
+				WriteDouble(row, 24, 40);
+				commandline[column - 3] = c;
+				WriteChar(c, row, column++);
+				WriteChar('_', row, column);
 				break;
 		}
-		WriteDouble(GetTicks(), 20, 60);
+		WriteDouble(GetTicks(), 24, 60);
 	}
 }
+
+char getchar(void)
+{
+	struct Message *kbdMsg;
+
+	kbdMsg = (struct Message *)sys_AllocMem(sizeof(struct Message));
+	kbdMsg->nextMessage = 0;
+	kbdMsg->byte = 1;
+	kbdMsg->quad = 0;
+	SendReceive(KbdPort, kbdMsg);
+	char c = kbdMsg->byte;
+	sys_DeallocMem(kbdMsg);
+	return c;
+}
+
