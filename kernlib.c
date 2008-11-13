b557bb4a364c3b6f3965bab2c34ebf6208aa20c3
ian <ian@bb4361fd-baa2-43a2-a164-8bb2ddc2f287>
2008-11-13 16:28:56 +0000

    Included a "row" parameter in KWriteHex

--- kernlib.c
+++ kernlib.c
@@ -68,7 +68,7 @@ void KWriteString(char * str, int row, int col)
 A kernel library function to write a quad character to the screen.
 ==========================================================================
 */
-void KWriteHex(long c) //, int row, int col)
+void KWriteHex(long c, int row) //, int col)
 {
 	char * VideoBuffer = (char *)0xB8000;
 
@@ -88,8 +88,8 @@ void KWriteHex(long c) //, int row, int col)
 		char hi = (c & 0xF0) >> 4;
 		hi += 0x30;
 		if (hi > 0x39) hi += 7;
-		VideoBuffer[28 - 4 * i] = hi;
-		VideoBuffer[30 - 4 * i] = lo;
+		VideoBuffer[160*row + 28 - 4 * i] = hi;
+		VideoBuffer[160*row + 30 - 4 * i] = lo;
 		c = c >> 8;
 	}
 	asm("pop %rsi");
