2c419bba3ef1d7ff8d7be20609db04c2caf0f58e
ian <ian@bb4361fd-baa2-43a2-a164-8bb2ddc2f287>
2008-11-16 22:13:25 +0000

    Starting to work on console task.

--- library/syscalls.h
+++ library/syscalls.h
@@ -4,6 +4,10 @@
 void * sys_AllocMem(long sizeRequested);
 void * sys_DeallocMem(void * memory);
 void sys_CreateTask(char * name);
+void sys_CreateKTask(void * code);
 void sys_KillTask(void);
+void sys_SendMessage(long port, struct Message * msg);
+void sys_SendReceiveMessage(long port, struct Message * msg);
+void sys_Sleep(int interval);
 
 #endif
