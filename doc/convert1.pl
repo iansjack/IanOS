49b91055eade15c868de0dd7b577d3d19f39f663
(no author) <(no author)@bb4361fd-baa2-43a2-a164-8bb2ddc2f287>
2012-04-06 21:15:02 +0000

    Made documentation compliant with XHTML standards.

--- doc/convert1.pl
+++ doc/convert1.pl
@@ -25,30 +25,32 @@ while (<INPUT>) {
 print $_;
 	while (/<</) {
 		s/<<A\s+([\w:\/\.\@\-]+)\s+(.+?)>>/<a href=\"$1\">$2<\/a>/;
-		s/<<PAR>>/<br><br>/;
-		s/<<HR>>/<hr class=\"normal\">/;
-		s/<<CONT>>/<td width=\"33.3\"><div class=\"contents\"><a href=\"code.html">Contents<\/a><\/div><\/td>/;
-		s/<<CONSTR>>/<span class=\"underconst\">This page is still under construction.<\/span><br>/;
+		s/<<PAR>>/<p>/;
+		s/<<HR>>/<hr class=\"normal\"><p>/;
+		s/<<CONT>>/<td><div class=\"contents\"><a href=\"code.html">Contents<\/a><\/div><\/td>/;
+		s/<<CONSTR>>/<span class=\"underconst\">This page is still under construction.<\/span><p>/; # <br>/;
 		s/<<C\s+(.+?)>>/<span class="code">$1<\/span>/;
 		s/<<S\s+(.+?)>>/<span class="sans">$1<\/span>/;
 		s/<<I\s+(.+?)>>/<span class="italic">$1<\/span>/;
+		s/<<B\s+(.+?)>>/<span class="bold">$1<\/span>/;
 		s/<<H1\s+(.+?)>>/<h1 class=\"normal\">$1<\/h1>/;
-		s/<<H([2-9])\s+(.+?)>>/<h$1>$2<\/h$1>/;
-		s/<<T\s+(.+?)>>/<!DOCTYPE HTML PUBLIC \"-\/\/W3C\/\/DTD HTML 4.01 Transitional\/\/EN\" \"http:\/\/www\.w3\.org\/TR\/html4\/loose.dtd\">\n<html>\n<head>\n<title>$1<\/title>\n<LINK REL=StyleSheet HREF=\"mystyle\.css\" TYPE=\"text\/css\" MEDIA=screen>\n<\/head>\n<body>/;
+		s/<<H([2-9])\s+(.+?)>>/<h$1>$2<\/h$1><p>/;
+		s/<<T\s+(.+?)>>/<!DOCTYPE HTML PUBLIC \"-\/\/W3C\/\/DTD HTML 4.01\/\/EN\" \"http:\/\/www\.w3\.org\/TR\/html4\/loose.dtd\">\n<html lang=\"en\">\n<head>\n<META http-equiv=\"Content-Style-Type\" content=\"text\/css\">\n<title>$1<\/title>\n<LINK REL=StyleSheet HREF=\"mystyle\.css\" TYPE=\"text\/css\" MEDIA=screen>\n<\/head>\n<body>/;
 		s/<<F\s+(.+?)>>/<a STYLE="text-decoration: none" href=\"HTML\/$assoc{$1}\"><span class=\"sans\">$1<\/span><\/a>/;
 		s/<<X\s+(.+?)>>/<a STYLE="text-decoration: none" href=\"HTML\/$assoc{$1}\"><span class=\"code\">$1<\/span><\/a>/;
 		s/<<D\s+(.+?)>>/<div class="displaycode">$1<\/div>/;
-		s/<<D\+\s+(.+?)>>/<div class="displaycode">$1<\/div><br>/;
+		s/<<D\+\s+(.+?)>>/<div class="displaycode">$1<\/div><p>/; # <br>/;
 		s/<<IN\s+(.+?)>>/<div class="indent">$1<\/div>/;
-		s/<<IN\+\s+(.+?)>>/<div class="indent">$1<\/div><br>/;
-		s/<<PREV\s+([\w:\/\.\@\-]+)\s+(.+?)>>/<td width=\"33.3%\"><div class=prev><a href=\"$1\"><b>Previous: <\/b>$2<\/a><\/div><\/td>/;
-		s/<<NEXT\s+([\w:\/\.\@\-]+)\s+(.+?)>>/<td width=\"33.3%\"><div class=next><a href=\"$1\"><b>Next: <\/b>$2<\/a><\/div><\/td>/;
-		s/<<NULL>>/<td width=\"33.3%\"><\/td>/;
-		s/<<NAVSTART>>/<table border="0" width="100%"><tr>/;
+		s/<<IN\+\s+(.+?)>>/<div class="indent">$1<\/div><p>/; # <br>/;
+		s/<<PREV\s+([\w:\/\.\@\-]+)\s+(.+?)>>/<td><div class=prev><a href=\"$1\"><span class=bold>Previous: <\/span>$2<\/a><\/div><\/td>/;
+		s/<<NEXT\s+([\w:\/\.\@\-]+)\s+(.+?)>>/<td><div class=next><a href=\"$1\"><span class=bold>Next: <\/span>$2<\/a><\/div><\/td>/;
+		s/<<NULL>>/<td><\/td>/;
+		s/<<NAVSTART>>/<table border="0"><tr>/;
 		s/<<NAVEND>>/<\/tr><\/table>/;
 	}
 	print OUTPUT $_;
 }
+print OUTPUT "<\/body>\n<\/html>";
 close INPUT;
 close OUTPUT;
 
