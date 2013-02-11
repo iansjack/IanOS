dfdeb366a60447fc35fdf0cdd734dc9bf3050542
Ian Jack <ian@ijack.org.uk>
2013-02-11 11:33:44 +0000

    A large number of changes have been mades as a result of running csplint on the code. More to follow....

---
+++ library/dummies.c
@@ -0,0 +1,19 @@
+int utime()
+{
+	return -1;
+}
+
+int chown()
+{
+	return -1;
+}
+
+int link()
+{
+	return -1;
+}
+
+int rmdir()
+{
+	return 0;
+}
---
+++ library/stat.c
@@ -0,0 +1,24 @@
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include "filesystem.h"
+
+#undef errno
+extern int errno;
+
+int stat(const char *filename, struct stat *st)
+{
+	int fd = open(filename, O_RDONLY);
+	if (fd)
+	{
+		fstat(fd, st);
+		errno = 0;
+		return 0;
+	}
+	else
+	{
+		errno = -ENOENT;
+		return 0;
+	}
+}
+
